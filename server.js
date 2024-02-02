34d7f0aff17bd06ff46b3e9c4f00e99e7271e9a9
fecredal40 <84052534+fecredal40@users.noreply.github.com>
2024-02-02 20:39:11 +0300

    a

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express()
+const PORT = 5000 
+
+ 
+
+
+
+
+app.get('/', (req,res) => {
+    res.send('test')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running... ${PORT}`)
+})
